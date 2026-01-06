b1355ce016530f209b5d7b01737038ce0e7386ff
Clara Jenhee Franco Aguilar <clara20franco@gmail.com>
2026-01-06 17:35:49 -0600

    cambio de icono por svg y mejor border para el chip

--- islaDelColor/extractorDeColor/extractor.js
+++ islaDelColor/extractorDeColor/extractor.js
@@ -308,7 +308,7 @@ function renderSelectedColors() {
     const copyBtn = document.createElement("button");
 copyBtn.className = "copy-btn";
 copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-  viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2"
+  viewBox="0 0 24 24" fill="none" stroke="#c933ffff" stroke-width="2"
   stroke-linecap="round" stroke-linejoin="round"
   class="icon icon-tabler icons-tabler-outline icon-tabler-copy">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -320,16 +320,26 @@ copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="
         .then(() => showToast("Color copiado"))
         .catch(() => showToast("No se pudo copiar el color"));
     };
+  // Botón eliminar
+  const removeBtn = document.createElement("button");
+  removeBtn.className = "remove-btn";
+  removeBtn.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+        viewBox="0 0 24 24" fill="none" stroke="#ff0000" stroke-width="2"
+        stroke-linecap="round" stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-x">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M18 6l-12 12" />
+      <path d="M6 6l12 12" />
+    </svg>
+  `;
 
-    // Botón eliminar
-    const removeBtn = document.createElement("button");
-    removeBtn.className = "remove-btn";
-    removeBtn.textContent = "❌";
-    removeBtn.onclick = () => {
-      selectedPoints.splice(idx, 1);
-      redrawAll();
-      renderSelectedColors();
-    };
+  removeBtn.onclick = () => {
+    selectedPoints.splice(idx, 1);
+    redrawAll();
+    renderSelectedColors();
+  };
+
 
     // Ensamblar chip
     chip.appendChild(colorDiv);
